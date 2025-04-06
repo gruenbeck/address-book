165b941ff1354913a6a808dfdbfe3f75bd48098b
Leon Grünbeck <leon.gruenbeck@connect.qut.edu.au>
2025-04-06 15:31:07 +1000

    Added data insertion for testing

--- src/main/java/com/example/addressbook/model/SqliteContactDAO.java
+++ src/main/java/com/example/addressbook/model/SqliteContactDAO.java
@@ -10,22 +10,41 @@ public class SqliteContactDAO implements IContactDAO {
     public SqliteContactDAO() {
         connection = SqliteConnection.getInstance();
         createTable();
+        // Used for testing, to be removed later
+        insertSampleData();
     }
 
-    public void createTable() {
+    private void createTable() {
+        // Create table if not exists
         try {
-            Statement createTable = connection.createStatement();
-            createTable.execute(
-                    "CREATE TABLE IF NOT EXISTS contacts ("
-                            + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                            + "firstName VARCHAR NOT NULL,"
-                            + "lastName VARCHAR NOT NULL,"
-                            + "phone VARCHAR NOT NULL,"
-                            + "email VARCHAR NOT NULL"
-                            + ")"
-            );
-        } catch (SQLException ex) {
-            System.err.println(ex);
+            Statement statement = connection.createStatement();
+            String query = "CREATE TABLE IF NOT EXISTS contacts ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "firstName VARCHAR NOT NULL,"
+                    + "lastName VARCHAR NOT NULL,"
+                    + "phone VARCHAR NOT NULL,"
+                    + "email VARCHAR NOT NULL"
+                    + ")";
+            statement.execute(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertSampleData() {
+        try {
+            // Clear before inserting
+            Statement clearStatement = connection.createStatement();
+            String clearQuery = "DELETE FROM contacts";
+            clearStatement.execute(clearQuery);
+            Statement insertStatement = connection.createStatement();
+            String insertQuery = "INSERT INTO contacts (firstName, lastName, phone, email) VALUES "
+                    + "('John', 'Doe', '0423423423', 'johndoe@example.com'),"
+                    + "('Jane', 'Doe', '0423423424', 'janedoe@example.com'),"
+                    + "('Jay', 'Doe', '0423423425', 'jaydoe@example.com')";
+            insertStatement.execute(insertQuery);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
